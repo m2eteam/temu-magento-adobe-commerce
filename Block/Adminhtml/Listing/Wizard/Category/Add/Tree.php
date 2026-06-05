e60fe633b7522b0c3db2c051fc8c2113c431afe5
deploy <deploy@m2e.team>
2026-06-05 07:31:55 +0000

    1.14.1 (FINAL RELEASE)

--- Block/Adminhtml/Listing/Wizard/Category/Add/Tree.php
+++ Block/Adminhtml/Listing/Wizard/Category/Add/Tree.php
@@ -178,7 +178,7 @@ class Tree extends \M2E\Temu\Block\Adminhtml\Listing\Category\Tree
         if ($node->hasChildren()) {
             $item['children'] = [];
 
-            if (!($node->getLevel() > 1 && !$isParent)) {
+            if (!($this->getUseAjax() && $node->getLevel() > 1 && !$isParent)) {
                 foreach ($node->getChildren() as $child) {
                     $item['children'][] = $this->_getNodeJson($child, $level + 1);
                 }
@@ -206,9 +206,13 @@ class Tree extends \M2E\Temu\Block\Adminhtml\Listing\Category\Tree
 
     public function buildNodeName($node): string
     {
-        return $this->escapeHtml($node->getName()) . <<<HTML
-<span category_id="{$node->getId()}">(0</span>{$this->__('of')} {$node->getProductCount()})
-HTML;
+        return sprintf(
+            '%s (<span category_id="%s">0</span> %s %s)',
+            $this->_escaper->escapeHtml($node->getName()),
+            $node->getId(),
+            __('of'),
+            $node->getProductCount()
+        );
     }
 
     public function getCategoryChildrenJson($categoryId)
--- Setup/UpgradeCollection.php
+++ Setup/UpgradeCollection.php
@@ -35,6 +35,7 @@ class UpgradeCollection extends \M2E\Core\Model\Setup\AbstractUpgradeCollection
             '1.12.1' => ['to' => '1.12.2', 'upgrade' => null],
             '1.12.2' => ['to' => '1.13.0', 'upgrade' => null],
             '1.13.0' => ['to' => '1.14.0', 'upgrade' => null],
+            '1.14.0' => ['to' => '1.14.1', 'upgrade' => null],
         ];
     }
 }
